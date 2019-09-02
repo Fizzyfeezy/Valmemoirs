ed369d937584a765a6e10f15cb7cba10e4c877ea
oyelami hafeez <oyelami.abdulhafeez@gmail.com>
2019-09-02 15:44:45 +0100

    Updates

---
+++ precache-manifest.bb4626d798177cce772d8e9818a2e9e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0669d66b77d72211edcb96f0adc9a23",
+    "url": "/Valmemoirs/index.html"
+  },
+  {
+    "revision": "5155e30c7ddb0466a9f6",
+    "url": "/Valmemoirs/static/css/2.a166031d.chunk.css"
+  },
+  {
+    "revision": "99b2ce40aa0de059ab4e",
+    "url": "/Valmemoirs/static/css/main.3e51d75b.chunk.css"
+  },
+  {
+    "revision": "5155e30c7ddb0466a9f6",
+    "url": "/Valmemoirs/static/js/2.e51b718f.chunk.js"
+  },
+  {
+    "revision": "99b2ce40aa0de059ab4e",
+    "url": "/Valmemoirs/static/js/main.2c17d819.chunk.js"
+  },
+  {
+    "revision": "28268bdeb9b794498bf3",
+    "url": "/Valmemoirs/static/js/runtime~main.6f3e8ab6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Valmemoirs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Valmemoirs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Valmemoirs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Valmemoirs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Valmemoirs/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2be4f3ab2b76352a49f58f943c2d47bc",
+    "url": "/Valmemoirs/static/media/love.2be4f3ab.png"
+  }
+]);
